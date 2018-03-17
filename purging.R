e654c7abcce5096464bf62b3be169ef84d2b4097
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2018-03-17 10:28:15 -0500

    Update purging.R

--- purging.R
+++ purging.R
@@ -1,6 +1,6 @@
 # See the README markdown for an overview of the example here. 
 
-# Read in the UN .csv data
+# Read in the UN.csv data
 
 # Start with simple bivariate specification
 UN$Contracept <- as.numeric(as.character(UN$Cont)) # character to generate "NA" instead of ".."
@@ -13,12 +13,12 @@ lm2 <- lm(Fert ~ FemEc + Contracept, data=UN); summary(lm2)
 # Each independently impacts Fert, but not together, suggesting mediation effects, which turns our original causal model to:  LABOR (indirect) -> CONTRACEPT (direct) -> FERTILITY
 m1 <- lm(Contracept ~ FemEc, data = UN); summary(m1)
 
-post <- UN[UN$Cont != "..", ] # create new dataset with only values (i.e., exclude "NAs")
+new.UN <- UN[UN$Cont != "..", ] # create new dataset excluding "NAs"
 
-post$purge <- m1$residuals # portion of "Contracept" independent of "FemEc"
+new.UN$purged <- m1$residuals # create "purged" variable, which is the portion of "Contracept" independent of "FemEc"
 
-m2 <- lm(Fert ~ FemEc + purge, data=post); summary(m2)
+# Now update the multivariate specification with the purged variable, and inspect the differences in magnitude and significance compared to lm2 
+m2 <- lm(Fert ~ FemEc + purged, data=new.UN); summary(m2)
 
 # Inspect the unique impacts picked up as a result of the purging strategy
-
-pairs(~ GDP + FemEc + purge, data = post, pch=19)
+pairs(~ GDP + FemEc + purged, data = new.UN, pch=19)
